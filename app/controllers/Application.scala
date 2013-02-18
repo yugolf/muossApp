82394f73c1e0a115e5d6f39ef44798d3617f3b18
yugolf <yugo.maede@gmail.com>
2013-02-18 21:05:40 +0900

    YOUR

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,7 @@ import play.api.mvc._
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your NEW APPLICATIONS is ready."))
+    Ok(views.html.index("YOUR NEW APPLICATIONS is ready."))
   }
 
 }
